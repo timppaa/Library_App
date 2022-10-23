86953672a3434f7dde8cadc96c8c1290b332e7d1
timppaa <tim-olsson@hotmail.com>
2022-10-23 19:08:46 +0200

    Created projects

---
+++ Library_Api/Controllers/BookController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        [HttpGet(Name = "SearchBook")]
+        public void Search()
+        {
+
+        }
+    }
+}
